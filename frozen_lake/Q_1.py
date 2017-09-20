3f6fd23e0eb171095995db6a8cda86108b9f73ce
yoshie <yoshie@g.ucla.edu>
2017-09-20 10:17:31 -0700

    ^^

--- frozen_lake/Q_1.py
+++ frozen_lake/Q_1.py
@@ -4,34 +4,51 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+#----------------------------------------------------------------------------------------------
+counter = {
+	-1:-1,
+	0:2,
+	1:3,
+	2:0,
+	3:1
+}
+
+NUM_EPOCH = 1
 
 class environment:
 
 	def __init__(self,problem):
 		self.problem = problem
 		self.env = gym.make(problem)
-		#env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1',force=True)
+		self.env = wrappers.Monitor(self.env, '/tmp/Q_2',force=True)
 	
 	def run(self, agent, sess, m):
 		s = self.env.reset()
+		#self.env.render()
+		a_prev = -1
 		#print ('s',s)
 		r_all = 0
 
 		while True:
-			self.env.render()
-			a = agent.act(s, sess, m)
+
+			#for i in range(16):
+			#	print np.argmax(sess.run(m.A2, feed_dict = {m.input_s:np.identity(16)[i:i+1]}))
+			
+			a = agent.act(s, sess, m, a_prev)
 			s_, r, done, info = self.env.step(a)
+			self.env.render()
 
 			if done:
 				s_ = None
-				self.env.render()
 			
 			agent.add_memory( (s, a, r, s_) )
-			agent.find_batch_to_train(sess,m)
+			for i in range(NUM_EPOCH):
+				agent.find_batch_to_train(sess,m)
 			
+			a_prev = a
 			s = s_
 			r_all += r
-			print("Total reward:", r_all)
+			#print("Total reward:", r_all)
 
 			if done:
 				return r_all
@@ -61,13 +78,17 @@ class agent:
 
 
 
-	def act(self, state, sess, m):
+	def act(self, state, sess, m, a_prev):
 		if random.random() < self.epsilon:
+			#print('-------random move------')
 			return random.randint(0, self.n_action-1)
 		else:
 			#print np.identity(self.n_state)[state:state+1]
-			Q_ = sess.run(m.Qout, feed_dict = {m.input_s:np.identity(self.n_state)[state:state+1]})
-			return np.argmax(Q_)
+			Q_ = sess.run(m.A2, feed_dict = {m.input_s:np.identity(self.n_state)[state:state+1]})
+			#print np.argmax(Q_[0]),a_prev
+			#if np.argmax(Q_[0]) == counter[a_prev]:
+			#	Q_[0][np.argmax(Q_)] = 0
+			return np.argmax(Q_[0])
 
 	def add_memory(self, sample):	# in (s, a, r, s_) format
 		self.memory.add(sample) 
@@ -78,8 +99,10 @@ class agent:
 		#print('epsilon',self.epsilon)
 
 	def find_batch_to_train(self, sess, m):
-		batch = self.memory.sample(BATCH_SIZE)
-		batch_len = len(batch)
+		#batch = self.memory.sample(BATCH_SIZE)
+		batches = self.memory.batches(BATCH_SIZE)
+		batches_len = len(batches)
+		batch_len = len(batches[0])
 		#print ('batchlen',batch_len)
 
 		no_state = np.zeros(self.n_state)
@@ -90,38 +113,36 @@ class agent:
 		p_ = sess.run(m.Qout, feed_dict = {m.input_s:np.identity(self.n_state)[states_:states_+1]})
 
 		'''
-		p  = []
-		p_ = []
-		for o in batch:
-			state  = o[0]
-			state_ = no_state if o[3] is None else o[3]
-			pp = sess.run(m.Qout, feed_dict = {m.input_s:np.identity(self.n_state)[state:state+1]})
-			pp_ = no_state if o[3] is None else sess.run(m.Qout, feed_dict = {m.input_s:np.identity(self.n_state)[state_:state_+1]})
-			p.append(pp)
-			p_.append(pp_)
+		x = np.zeros((batches_len,batch_len, self.n_state))
+		y = np.zeros((batches_len,batch_len, self.n_action))
 
-		#print max(p_[0][0]) #(1,n_action)
-		
-		x = np.zeros((batch_len, self.n_state))
-		y = np.zeros((batch_len, self.n_action))
-        	
-		for i in range(batch_len):
-			o = batch[i]
-			#print ('o',o)
-			s = o[0]; a = o[1]; r = o[2]; s_ = o[3]
-            
-			t = p[i][0]
-			if s_ is None:
-				t[a] = r
-			else:
-				t[a] = r + GAMMA * max(p_[i][0])
+		for j in range(batches_len):
+			batch = batches[j]
+			for i in range(batch_len):
+				o = batch[i]
+				state  = o[0]
+				state_ = no_state if o[3] is None else o[3]
+				pp = sess.run(m.A2, feed_dict = {m.input_s:np.identity(self.n_state)[state:state+1]})
+				pp_ = no_state if o[3] is None else sess.run(m.A2, feed_dict = {m.input_s:np.identity(self.n_state)[state_:state_+1]})
 
-			x[i] = np.identity(self.n_state)[s:s+1]
-			y[i] = t
+				s = o[0]; a = o[1]; r = o[2]; s_ = o[3]
+				t = pp[0]
+				if s_ is None:
+					t[a] = r
+				#	if s == 14:
+				#		t[a] = r
+				#	else:
+				#		t[a] = r-1
+				else:
+					t[a] = r + GAMMA * max(pp_[0])
+
+				x[j][i] = np.identity(self.n_state)[s:s+1]
+				y[j][i] = t	
 
 		#----train(x, y, sess)
 		#print('x,y',x,y)
-		sess.run(m.updateModel,feed_dict = {m.input_s:x, m.nextQ:y})
+		for j in range(batches_len):
+			sess.run(m.updateModel,feed_dict = {m.input_s:x[j], m.nextQ:y[j]})
 
 
 class memory:
@@ -139,6 +160,13 @@ class memory:
 	def sample(self, n):
 		n = min(n, len(self.samples))
 		return random.sample(self.samples, n)
+	def batches(self,n):
+		batches_ = []
+		n = min(n, len(self.samples))
+		for i in range(len(self.samples)/n):
+			batches_.append(self.samples[i*n+len(self.samples)%n:(i+1)*n+len(self.samples)%n])
+		#batches_.append(self.samples[-(self.samples%n):])
+		return batches_
 
 
 class model:
@@ -148,21 +176,29 @@ class model:
 		self.n_action = n_action
 
 		self.input_s = tf.placeholder(shape=[None,n_state],dtype=tf.float32)
-		self.w1 = tf.Variable(tf.random_uniform([n_state,n_action],0,0.01))
-		self.Qout = tf.matmul(self.input_s,self.w1)
-		#self.b1 = tf.Variable(tf.random_uniform([1,n_action],0,0.01))
-		#self.Qo = tf.add(tf.matmul(self.input_s,self.w1), self.b1)
+		self.w1 = tf.Variable(tf.random_normal([n_state,16],0,0.2))
+		#self.Qout = tf.matmul(self.input_s,self.w1)
+		self.b1 = tf.Variable(tf.random_normal([1,16],0,0.2))
+		self.h1 = tf.add(tf.matmul(self.input_s,self.w1), self.b1)
+		self.A1 = tf.nn.relu(self.h1)
+		
+		self.w2 = tf.Variable(tf.random_normal([16,n_action],0,0.2))
+		self.b2 = tf.Variable(tf.random_uniform([1,n_action],0,0.2))
+		self.h2 = tf.add(tf.matmul(self.A1,self.w2),self.b2)
+		self.A2 = (self.h2)
+		#self.h2 = tf.matmul(self.A1,self.w2)
+		
 		#self.Qout = tf.nn.softmax(self.Qo)
-		self.predict = tf.argmax(self.Qout,1)
 
 		#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
 		self.nextQ = tf.placeholder(shape=[None,n_action],dtype=tf.float32)
-		self.loss = tf.reduce_sum(tf.square(self.nextQ - self.Qout))
+		self.loss = tf.reduce_sum(tf.square(self.nextQ - self.A2))
+		#self.loss = tf.losses.huber_loss(labels = self.nextQ, predictions = self.A2, delta = )
 
 		#if not is_training:
 		#	return
 
-		self.trainer = tf.train.GradientDescentOptimizer(learning_rate=0.00025)
+		self.trainer = tf.train.GradientDescentOptimizer(learning_rate=0.0025)
 		self.updateModel = self.trainer.minimize(self.loss)
 
 
@@ -183,18 +219,27 @@ if __name__ == "__main__":
 
 	n_state  = env.env.observation_space.n
 	n_action = env.env.action_space.n
+	episode  = 100
 
 	agent_ = agent(n_state, n_action)
 
 	r_all  = 0
-	
+	R = []
 	with tf.Session() as sess:
 		model_ = model(n_state,n_action, sess)
 		sess.run(model_.init)
-		for i in range(100):
+		for i in range(episode):
 			r_ = env.run(agent_, sess, model_)
+
 			r_all += r_
-		print ('ratio = ',r_all/100)
+			R.append(r_all)
+
+		for i in range(n_state):
+			print np.argmax(sess.run(model_.A2, feed_dict = {model_.input_s:np.identity(n_state)[i:i+1]}))
+			
+		print ('ratio = ',r_all/episode)
+		plt.plot(R)
+		plt.show()
 
 
 
